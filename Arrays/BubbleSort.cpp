2b420ce1ce1263f816e930b888aff5f0230f335a
HIMANSHU NEGI <145410844+HIMANSHUNEGI2004@users.noreply.github.com>
2025-10-08 20:43:18 +0530

    Add files via upload

--- Arrays/BubbleSort.cpp
+++ Arrays/BubbleSort.cpp
@@ -2,38 +2,31 @@
 using namespace std;
 
 void BubbleSort(int arr[],int n){
-
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-1-i;j++){
-            if(arr[j]>arr[j+1]){
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n+i-1;j++){
+            if(arr[j]>arr[j+1])
+            {
+                int temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
             }
         }
     }
-}
-void Display(int arr[],int n){
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
+
 }
 
 int main(){
-    int arr[6]={4,5,6,3,1,2};
-    Display(arr,6);
-    BubbleSort(arr,6);
+    int arr[6]={2,3,1,5,4,0};
+    for(int i=0;i<6;i++){
+        cout<<arr[i] <<" ";
+    }
     cout<<endl;
-    Display(arr,6);
+    BubbleSort(arr,6);
 
+    for(int i=0;i<6;i++){
+        cout<<arr[i] <<" ";
+    }
     return 0;
 }
-// 4 5 6 3 1 2  i=0 j =0
-// 4 5 3 6 1 2 
-// 4 5 3 1 6 2  
-// 4 5 3 1 2 6
 
-// 4 5 3 1 2 6  i=1  j=0
-// 4 3 5 1 2 6
-// 4 3 1 5 2 6
-// 4 3 1 2 5 6 
+
--- Arrays/InsertionSort.cpp
+++ Arrays/InsertionSort.cpp
@@ -2,27 +2,46 @@
 using namespace std;
 
 void InsertionSort(int arr[],int n){
+    for(int i=0;i<n;i++)
+    {
+        int j=i;
+        cout<<"i="<< i <<": "<<endl;
+        while(j>0 && arr[j]<arr[j-1])
+        {
+            cout<<"Original :";
+            for(int k= 0; k<i+1; k++){
+                cout<<arr[k] <<" ";
+            }
+            cout<<endl;
+
+            swap(arr[j],arr[j-1]);
+            j--;
+            
+            cout<<"After Swap :";
+            for(int y=0; y<i; y++){
+                cout<<arr[y] <<" ";
+            }
+            cout<<endl;
+        }
+    }
+}
+
+int main(){
+    int arr[6]={2,3,1,5,4,0};
     for(int i=0;i<6;i++){
         cout<<arr[i] <<" ";
     }
-    cout <<endl;
-    int k;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(arr[j]>arr[j+1])
-            {
-                k=arr[j+1];
-                arr[j+1]=arr[j];
-                j--;
-            }
-            else{
-                arr[j]=key;
-            }
+    cout<<endl;
+    InsertionSort(arr,6);
 
-
+    for(int i=0;i<6;i++){
+        cout<<arr[i] <<" ";
+    }
+    return 0;
 }
 
-// 5 3 4 2 0 1 j=0
-// 3 5 4 2 0 1 j=1
+
+// 5 3 4 2 0 1 j=0 i=0
+// 3 5 4 2 0 1 j=1 i=1
 // 3 4 5 2 0 1 j=2
 // 
